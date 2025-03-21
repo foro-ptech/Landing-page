25f07c9b51db7050b1e30927281e6085991196a8
Cubos <cubos2769.andrade@gmail.com>
2025-03-21 13:19:47 -0600

    images implemented

--- src/components/Elements.jsx
+++ src/components/Elements.jsx
@@ -1,4 +1,7 @@
 import '../styles/Elements.css'
+import laptop from '/Laptop.gif'
+import hospital from '/hospital_icon.png'
+import person from '/personas.png'
 
 function Elements() {
   return (
@@ -6,19 +9,19 @@ function Elements() {
       <h3 className='subtitle'>Propuesta de valor</h3>
       <div className='card-container'>
         <div className='card'>
-          <img src="" alt="Imagen no encontrada"/>
+          <img src={person} alt="Imagen no encontrada" className='image'/>
           <h3>Titulo</h3>
           <p>Descripcion Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut dicta quisquam, illum vero</p>
         </div>
 
         <div className='card'>
-          <img src="" alt="Imagen no encontrada"/>
+          <img src={laptop} alt="Imagen no encontrada" className='image'/>
           <h3>Titulo</h3>
-          <p>Descripcion Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut dicta quisquam, illum vero</p>
+          <p className='lap-p'>Descripcion Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut dicta quisquam, illum vero</p>
         </div>
 
         <div className='card'>
-          <img src="" alt="Imagen no encontrada"/>
+          <img src={hospital} alt="Imagen no encontrada" className='image'/>
           <h3>Titulo</h3>
           <p>Descripcion Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut dicta quisquam, illum vero</p>
         </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../styles/Footer.css';
+import facebook from '/facebook.png'
+import insta from '/instagram.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <h4 className='contact-h4'>Contact us</h4>
       <div className='contact'>
+        <h4 className='contact-h4'>Contact us</h4>
         <p className='footer-p'>Phone <br /><a href="">33 1234 5678</a></p>
         <p className='footer-p'>Email <br /><a href="">hello@reallygreatsite.com</a></p>
-        <div className='social-div'>
-          <p className='social'>Social</p>
-          <img src="" alt="" /><img src="" alt="" />
-        </div>
       </div>
+      <div className='social-div'>
+          <h4 className='contact-h4'>Social</h4>
+          <div>
+            <a href="facebook.com"><img src={facebook} alt="" className='icon'/></a>
+            <a href="instagram.com"><img src={insta} alt="" className='icon'/></a>
+          </div>
+        </div>
     </footer>
   );
 };
